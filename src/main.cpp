b6961751bfe9466ae5636ee941c2a49be0df63fb
Dmitry Vostrykov <38708105+Dimkarus@users.noreply.github.com>
2019-03-25 06:31:39 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "auto.h"
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	std::vector<std::string> menu = { "Cap", "Tea", "Coffe","Espresso" };
+	std::vector<double> price = { 30.0, 20.0, 25.0, 35.0 };
+	Automata tmp(price, menu);
+	tmp.on();
+	tmp.coin();
+}
