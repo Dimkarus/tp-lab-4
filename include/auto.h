67086d9f3ac5e0a2ff25d0c64d55f80a3a31068f
Dmitry Vostrykov <38708105+Dimkarus@users.noreply.github.com>
2019-03-25 06:32:58 +0300

    Add files via upload

---
+++ include/auto.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class Automata
+{
+private:
+
+	std::vector<double> price;
+	std::vector<std::string> menu;
+	double cash;
+	enum STATES {
+		OFF,
+		WAIT,
+		ACCEPT,
+		CHECK,
+		COOK
+	};
+	STATES state;
+public:
+	Automata(const std::vector<double> &tmp_price,
+		const std::vector<std::string> &tmp_menu);
+	void on();
+	void off();
+	void coin();
+	void printMenu();
+	void printState();
+	void cancel();
+	void choice();
+	bool check(double tmp_price);
+	void cook(const std::string &tmp_menu);
+	void finish();
+};
