f49cfa0d76ac893810b06a29ceb87c51a5d053a2
Dmitry Vostrykov <38708105+Dimkarus@users.noreply.github.com>
2019-03-25 12:18:53 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include "auto.h"
+#include "../include/auto.h"
 
 TEST(lab4, task_1) {
 	std::vector<std::string> menu = { "Cap", "Tea", "Coffe","Espresso" };
@@ -23,4 +23,22 @@ TEST(lab4, task_2) {
     EXPECT_EQ(1, tmp);
 }
 
+TEST(lab4, task_3) {
+
+    int tmp = 10;
+    EXPECT_EQ(10, tmp);
+}
+
+
+TEST(lab4, task_4) {
+ 
+    int tmp = 5;
+    EXPECT_EQ(5, tmp);
+}
+
+TEST(lab4, task_5) {
+  
+    int tmp = 0;
+    EXPECT_EQ(0, tmp);
+}
 
