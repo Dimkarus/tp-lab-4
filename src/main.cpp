836efdcfe6b9c31a5a2b847d19cf53cf084c2c0e
Dmitry Vostrykov <38708105+Dimkarus@users.noreply.github.com>
2019-03-29 02:40:44 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	std::vector<std::string> menu = { "Cap", "Tea", "Coffe","Espresso" };
 	std::vector<int> price = { 30, 20, 25, 35 };
-	Automata tmp(price, menu);
+	Automata tmp;
 	tmp.on();
 	tmp.coin(10);
 }
